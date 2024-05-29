7cdd79f7fca1b733ba433ab07d45e9fbadc30dd2
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2024-05-29 22:54:29 +0700

    Create-header-compoment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import  Home from "./pages/Home";
-import  About  from "./pages/Home";
-import  Profile  from "./pages/Profile";
-import  SignIn  from "./pages/SignIn";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./compoments/Header";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
 import SignOut from "./pages/SignOut";
 
 function App() {
   return (
-    <BrowserRouter >
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-in" element={<SignOut />} />
-    </Routes>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignOut />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
