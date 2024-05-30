ca55fbb0766b4a036e234cbfe119d6187f73923e
NguyenPhucTrong <95624365+NguyenPhucTrong@users.noreply.github.com>
2024-05-30 17:15:26 +0700

    Create user model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.error(err));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
